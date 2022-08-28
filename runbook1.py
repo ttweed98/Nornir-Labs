0b0a74b1a0817ef784a8f78015c83527466c79c9
ttweed98 <ttweed98gmail.com>
2022-08-28 16:14:20 +0000

    Added new files

--- runbook1.py
+++ runbook1.py
@@ -1,14 +1,21 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
-
+import ipdb
+from rich import print as rprint
 nr = InitNornir(config_file="config.yaml")
 
-commands = input("\nEnter Commands You Wish To Send: ")
-cmds = commands.split(",")
 
-def push_show_commands(task):
-    for cmd in cmds:
-        task.run(task=send_command, command=cmd)
-results = nr.run(task=push_show_commands)
-print_result(results)
+def pull_structured_data(task):
+    version_result = task.run(task=send_command, command="show version")
+    task.host["facts"] = version_result.scrapli_response.genie_parse_output()
+    version_number = task.host["facts"]["version"]["version_short"]
+    if version_number == "15.9":
+        rprint(f"{task.host}: [green]VERSION CHECK PASSED[/green]")
+    else:
+        rprint(f"{task.host}: [red]VERSION CHECK FAILED[/red]")
+
+
+results = nr.run(task=pull_structured_data)
+# print_result(results)
+# ipdb.set_trace()
